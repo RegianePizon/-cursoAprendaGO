d5691288e540397cac063e9cc19ca7db45544e7f
Regiane Pizon <44241434+RegianePizon@users.noreply.github.com>
2023-05-24 18:19:38 -0300

    Update treinamentoGO.go

--- treinamentoGO.go
+++ treinamentoGO.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+/*- Alem da goroutine principal, crie duas outras goroutines.
+- Cada goroutine adicional devem fazer um print separado.
+- Utilize waitgroups para fazer com que suas goroutines finalizem antes de o programa terminar.
+*/
+
+var wg sync.WaitGroup //WaitGroup serve para esperar que uma coleção de goroutines termine sua execução
 
 func main() {
 
-	fmt.Println("Hola mundo")
+	wg.Add(2) //"Quantas goroutines?"
+
+	go Goroutine1()
+	go Goroutine2()
+
+	wg.Wait() //"Espera todo mundo terminar."
+
+}
+
+func Goroutine1() {
+	fmt.Println("Eu sou o Goroutine numero 1")
+
+	wg.Done() //"Deu!"
+}
+
+func Goroutine2() {
+	fmt.Println("Eu sou o Goroutine numero 2")
+
+	wg.Done() //"Deu!"
 }
